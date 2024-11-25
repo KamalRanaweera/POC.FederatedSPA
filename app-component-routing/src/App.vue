f7f915f61626ef4880a4d3b19b3df07846e33ae8
Kamal Ranaweera <ranaweer@ualberta.ca>
2024-11-24 18:18:07 -0700

    Code clean-up.

--- app-component-routing/src/App.vue
+++ app-component-routing/src/App.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+      </nav>
+    </div>
+  </header>
+
   <RouterView />
 </template>
-
--- app-component-routing/src/router/index.ts
+++ app-component-routing/src/router/index.ts
@@ -9,14 +9,6 @@ const router = createRouter({
       component: () => import('../components/RoutingComponentMain.vue')
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
       path: '/app-component-c/sub-route-one',
       name: 'sub-route-one',
       component: () => import("../components/SubRouteOne.vue"),
